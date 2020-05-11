d3b8d5005b1780f82a4f4ae12e19097980e6be7c
root <root@ip-192-168-1-15.ap-south-1.compute.internal>
2020-05-11 03:13:13 +0000

    added main.js file

---
+++ main.js
@@ -0,0 +1,13 @@
+// load the http module
+var http = require('http');
+
+// configure our HTTP server
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World!! \n");
+});
+
+// listen on localhost:3000
+server.listen(3000);
+console.log("Server listening on port 3000 :  http://127.0.0.1:3000/");
+
